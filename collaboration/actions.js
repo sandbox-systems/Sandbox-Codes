b8dbcd7f1207fe5570bc6a667dbc527b99bdd57c
Shriggs <spm1215@gmail.com>
2018-06-27 16:48:29 -0500

    Modularize composition of user data that is sent to clients from server

--- collaboration/actions.js
+++ collaboration/actions.js
@@ -1,3 +1,4 @@
+var mongodb = require('mongodb');
 var queries = require("./queries");
 
 module.exports = {
@@ -22,6 +23,103 @@ module.exports = {
 
         pub.events.emit("emitEasyrtcMsg", conObj, msgType, msg, socketCb, function (err) {
             module.exports.callback(pub, conObj.getEasyrtcid(), err, "Successfully sent data to client");
+        });
+    },
+    getObjectIDList: function(ids) {
+        var idObjs = [];
+
+        for (var i = 0; i < ids.length; i++) {
+            idObjs.push(mongodb.ObjectID(ids[i]));
+        }
+
+        return idObjs;
+    },
+    // callback = function (userData, friendIDs, user)
+    getUserData: function (db, username, callback) {
+        // Get user object with appropriate username
+        queries.getUser(db, username, function (user) {
+            // Once user is fetched, only the id and name are sent to the client
+            var userData = {};
+            userData.id = user._id.toString();
+            userData.name = user.name;
+
+            // Friend IDs must be cast to ObjectID objects to use in querying
+            var friendIDs = module.exports.getObjectIDList(user.friends);
+
+            callback(userData, friendIDs, user);
+        });
+    },
+    // callback = function (friendData, roomIDs)
+    getFriendData: function (db, user, userPool, friendIDs, callback) {
+        // Fetch an array of user objects with appropriate IDs corresponding to user friend IDs
+        queries.getUsers(db, friendIDs, function (friends) {
+            // Once friends are fetched, only the id, username, and name are sent to the client
+            var friendData = [];
+
+            for (var f = 0; f < friends.length; f++) {
+                userPool.push(friends[f]._id.toString());
+                friendData.push({
+                    id: friends[f]._id.toString(),
+                    uname: friends[f].username,
+                    name: friends[f].name
+                });
+            }
+
+            // Room IDs must be cast to ObjectID objects to use in querying
+            var roomIDs = module.exports.getObjectIDList(user.roomIDs);
+
+            callback(friendData, roomIDs);
+        });
+    },
+    // callback = function (roomData)
+    getRoomData: function (db, userData, userPool, roomIDs, callback) {
+        // Fetch an array of room objects with appropriate room IDs
+        queries.getRooms(db, roomIDs, function (rooms) {
+            var roomData = [];
+            var newMembers = {};
+
+            // Once the rooms ae fetched, only the id, name, members, and chat entries are sent to the client
+            for (var i = 0; i < rooms.length; i++) {
+                var room = {
+                    id: rooms[i]._id.toString(),
+                    name: rooms[i].name,
+                    members: [],
+                    chats: rooms[i].chatEntries
+                };
+                // If the member is also a friend, add the already created friend to members
+                // Otherwise, the other members must be fetched from the DB
+                for (var j = 0; j < rooms[i].members.length; j++) {
+                    var member = rooms[i].members[j];
+                    if (member !== userData.id) {
+                        if (userPool.includes(member)) {
+                            room.members.push(member);
+                        } else {
+                            newMembers[member] = i;
+                            userPool.push(member);
+                        }
+                    }
+                }
+                roomData.push(room);
+            }
+
+            // New member IDs must be cast to ObjectID objects to use in querying
+            var memberIDs = module.exports.getObjectIDList(Object.keys(newMembers));
+
+            // Fetch an array of user objects with appropriate IDs corresponding to room new member IDs
+            queries.getUsers(db, memberIDs, function (members) {
+                // Once new members are fetched, only the id, username, and name are sent to the client
+                for (var i = 0; i < members.length; i++) {
+                    var member = {
+                        id: members[i]._id.toString(),
+                        uname: members[i].username,
+                        name: members[i].name
+                    };
+                    // Room index in roomData was stored so the member can be added to the appropriate room
+                    roomData[newMembers[member.id]].members.push(member);
+                }
+
+                callback(roomData);
+            });
         });
     }
 };
--- collaboration/server.js
+++ collaboration/server.js
@@ -54,96 +54,18 @@ var easyrtcServer = easyrtc.listen(httpApp, io, null, function (error, pub) {   
             // Contains all processed user IDs
             var userPool = [];
 
-            // Get user object with appropriate username
-            queries.getUser(db, username, function (user) {
-                // Once user is fetched, only the id and name are sent to the client
-                var userData = {};
-                userData.id = user._id.toString();
-                userData.name = user.name;
+            actions.getUserData(db, username, function (userData, friendIDs, user) {
+                actions.getFriendData(db, user, userPool, friendIDs, function (friendData, roomIDs) {
+                    actions.getRoomData(db, userData, userPool, roomIDs, function (roomData) {
+                        // Create single object to encapsulate all data
+                        var data = {
+                            userData: userData,
+                            friendData: friendData,
+                            roomData: roomData
+                        };
 
-                // Friend IDs must be cast to ObjectID objects to use in querying
-                var friendIDs = [];
-                for (var i = 0; i < user.friends.length; i++) {
-                    friendIDs.push(mongodb.ObjectID(user.friends[i]));
-                }
-
-                // Fetch an array of user objects with appropriate IDs corresponding to user friend IDs
-                queries.getUsers(db, friendIDs, function (friends) {
-                    // Once friends are fetched, only the id, username, and name are sent to the client
-                    var friendData = [];
-                    for (var f = 0; f < friends.length; f++) {
-                        userPool.push(friends[f]._id.toString());
-                        friendData.push({
-                            id: friends[f]._id.toString(),
-                            uname: friends[f].username,
-                            name: friends[f].name
-                        });
-                    }
-
-                    // Room IDs must be cast to ObjectID objects to use in querying
-                    var roomIDs = [];
-                    for (var i = 0; i < user.roomIDs.length; i++) {
-                        roomIDs.push(mongodb.ObjectID(user.roomIDs[i]));
-                    }
-
-                    // Fetch an array of room objects with appropriate room IDs
-                    queries.getRooms(db, roomIDs, function (rooms) {
-                        var roomData = [];
-                        var newMembers = {};
-
-                        // Once the rooms ae fetched, only the id, name, members, and chat entries are sent to the client
-                        for (var i = 0; i < rooms.length; i++) {
-                            var room = {
-                                id: rooms[i]._id.toString(),
-                                name: rooms[i].name,
-                                members: [],
-                                chats: rooms[i].chatEntries
-                            };
-                            // If the member is also a friend, add the already created friend to members
-                            // Otherwise, the other members must be fetched from the DB
-                            for (var j = 0; j < rooms[i].members.length; j++) {
-                                var member = rooms[i].members[j];
-                                if (member !== userData.id) {
-                                    if (userPool.includes(member)) {
-                                        room.members.push(member);
-                                    } else {
-                                        newMembers[member] = i;
-                                        userPool.push(member);
-                                    }
-                                }
-                            }
-                            roomData.push(room);
-                        }
-
-                        // New member IDs must be cast to ObjectID objects to use in querying
-                        var memberIDs = [];
-                        for (var k = 0; k < Object.keys(newMembers).length; k++) {
-                            memberIDs.push(mongodb.ObjectID(Object.keys(newMembers)[k]));
-                        }
-
-                        // Fetch an array of user objects with appropriate IDs corresponding to room new member IDs
-                        queries.getUsers(db, memberIDs, function (members) {
-                            // Once new members are fetched, only the id, username, and name are sent to the client
-                            for (var i = 0; i < members.length; i++) {
-                                var member = {
-                                    id: members[i]._id.toString(),
-                                    uname: members[i].username,
-                                    name: members[i].name
-                                };
-                                // Room index in roomData was stored so the member can be added to the appropriate room
-                                roomData[newMembers[member.id]].members.push(member);
-                            }
-
-                            // Create single object to encapsulate all data
-                            var data = {
-                                userData: userData,
-                                friendData: friendData,
-                                roomData: roomData
-                            };
-
-                            // Finally, the data is sent to the client
-                            actions.emitMsgToClient(pub, conObj, "userData", data);
-                        });
+                        // Finally, the data is sent to the client
+                        actions.emitMsgToClient(pub, conObj, "userData", data);
                     });
                 });
             });

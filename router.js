223f21ffc91541fc81a5f89cf738357f527a7d7a
Shriggs <spm1215@gmail.com>
2018-07-26 15:38:47 -0500

    Add routing system

---
+++ router.js
@@ -0,0 +1,31 @@
+/**
+ * Route file manager paths to respective templates and requests
+ *
+ * @author Shivashriganesh Mahato
+ */
+
+let castleApp = angular.module('castle', ['ngRoute']);
+
+castleApp.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/Castle.html', {
+            redirectTo: '/home'
+        })
+        .when('/home', {
+            templateUrl: 'home.html'
+        })
+        .when('/playground', {
+
+        })
+        .when('/treasury', {
+            templateUrl: 'FileM.html',
+        })
+        .when('/chat', {
+            templateUrl: 'chat.html',
+        })
+        .otherwise({
+            // 404
+        });
+
+    $locationProvider.html5Mode(true);
+});

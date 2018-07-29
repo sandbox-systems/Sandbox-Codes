456d81221c105b59a8adba3c6ae280645573a9d5
Shivashriganesh Mahato <spm1215@gmail.com>
2018-07-29 15:42:19 -0500

    playground.php commit after styling updates

--- Sandbox_Back/playground.php
+++ Sandbox_Back/playground.php
@@ -896,7 +896,25 @@
 				$("#fileRefresh").on("click",function(){
 					scan();
 				});
-				
+
+                /****************************************************
+                 ****************** TAB BINDINGS ********************
+                 ****************************************************/
+                var tabID = 0;
+                var numTabs = 0;
+                function newTab(){
+                    numTabs++;
+                    tabID = numTabs;
+                    $('#tab-list').append($('<li><a href="#tab' + tabID + '" role="tab" data-toggle="tab">Tab ' + tabID + '<button class="close" type="button" title="Remove this page">×</button></a></li>'));
+                    $('#tab-content').append($('<div class="tab-pane fade" id="tab' + tabID + '">Tab '+ tabID +' content</div>'));
+                    return tabID;
+                }
+                $('#tab-list').on('click','.close',function(){
+                    var tabID = $(this).parents('a').attr('href');
+                    $(this).parents('li').remove();
+                    //$(tabID).remove();
+                });
+
 				/****************************************************
 				 ****************** KEY BINDINGS ********************
 				 ****************************************************/
@@ -945,23 +963,6 @@
                     },
                 });
             }
-            $(document).ready(function () {
-                var tabID = 1;
-                $('#btn-add-tab').click(function () {
-                    tabID++;
-                    $('#tab-list').append($('<li><a href="#tab' + tabID + '" role="tab" data-toggle="tab">Tab ' + tabID + '<button class="close" type="button" title="Remove this page">×</button></a></li>'));
-                    //$('#tab-content').append($('<div class="tab-pane fade" id="tab' + tabID + '">Tab '+ tabID +' content</div>'));
-                });
-                $('#tab-list').on('click','.close',function(){
-                    var tabID = $(this).parents('a').attr('href');
-                    $(this).parents('li').remove();
-                    $(tabID).remove();
-
-                    //display first tab
-                    var tabFirst = $('#tab-list a:first');
-                    tabFirst.tab('show');
-                });
-            });
 		</script>
 	</body>
 </html>

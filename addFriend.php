0886af9cdfddc06bcbf5ea0d09cfa4d6ea8bc478
Shriggs <spm1215@gmail.com>
2018-07-30 15:55:17 -0500

    Put add friend button in searchbox results
    Also add changes to addFriend.php that weren't added in last commit

---
+++ addFriend.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'fileManager/initDB.php';
+
+session_start();
+
+$fromID = $_SESSION['object_id'];
+$fromUsername = $_SESSION['username'];
+$fromName = $_SESSION['name'];
+
+$toUname = $_POST['user'];
+$to = getDocuments($man, "users", ['username' => $toUname], [])[0];
+
+$request = array(
+    "type" => "friend",
+	"from" => array(
+		"id" => $fromID,
+		"uname" => $fromUsername,
+		"name" => $fromName
+    ),
+	"fromID" => $fromID,
+	"to" => (string) $to->_id,
+	"accepted" => null
+);
+
+insertDocument($man, "requests", $request);

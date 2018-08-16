a14e0b0aad166a5f24a5545cf3735089e99852e0
Shivashriganesh Mahato <spm1215@gmail.com>
2018-08-16 17:30:34 -0500

    Add delete_repo to scope

--- fileManager/github/sync.php
+++ fileManager/github/sync.php
@@ -8,4 +8,4 @@
 include 'params.php';
 
 $wasSuccessful = !isset($_GET['unsuccessful']);
-$url = "https://github.com/login/oauth/authorize?scope=repo&client_id=$client_id";
+$url = "https://github.com/login/oauth/authorize?scope=repo delete_repo&client_id=$client_id";
